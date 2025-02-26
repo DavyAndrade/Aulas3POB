498dacbf873f254f80198ddb675ac9d59438f741
DavyAndrade <davy.dama55@gmail.com>
2025-02-26 10:45:13 -0300

    First Commit

---
+++ exercicio01/problema2/calcularAreaCirculo.java
@@ -0,0 +1,29 @@
+/*
+2. Área de um Círculo
+Desenvolva um programa que leia o raio de um círculo e calcule sua área.
+
+Fórmula: Área = π * raio²
+
+Considere π = 3.14159.
+*/
+
+package exercicio01.problema2;
+
+import java.util.Scanner;
+
+public class calcularAreaCirculo {
+
+    public static double calcularArea(double raio) {
+        return Math.PI * Math.pow(raio, 2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o raio do círculo: ");
+        double raio = sc.nextDouble();
+
+        double area = calcularArea(raio);
+
+        System.out.printf("A área do círculo é %.2f%n", area);
+    }
+}
---
+++ exercicio01/problema3/conversaoTemperatura.java
@@ -0,0 +1,22 @@
+/*
+3. Conversão de Temperatura
+Crie um programa que leia uma temperatura em graus Celsius e a converta para Fahrenheit.
+
+Fórmula: F = (C × 9/5) + 32.
+
+*/
+
+package exercicio01.problema3;
+
+public class conversaoTemperatura {
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+    public static void main(String[] args) {
+        double celsius = 32.0;
+        double fahrenheit = celsiusToFahrenheit(celsius);
+
+        System.out.printf("%.2f graus Celsius é igual a %.2f graus Fahrenheit%n", celsius, fahrenheit);
+    }
+}
