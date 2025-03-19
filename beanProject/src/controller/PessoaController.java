84096e180d07515da0754853bf5646ca090f70d0
DavyAndrade <davy.dama55@gmail.com>
2025-03-19 10:48:03 -0300

    Bean Project

---
+++ beanProject/src/controller/PessoaController.java
@@ -0,0 +1,33 @@
+package beanProject.src.controller;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import beanProject.src.domain.Pessoa;
+
+public class PessoaController {
+    public void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Entre com o nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Entre com a idade: ");
+        int idade = sc.nextInt();
+
+        System.out.print("Entre com a altura: ");
+        double altura = sc.nextDouble();
+
+        System.out.print("Entre com o peso: ");
+        double peso = sc.nextDouble();
+
+        System.out.print("Entre com o sexo (M/F): ");
+        char sexo = sc.next().charAt(0);
+
+        Pessoa pessoa = new Pessoa(nome, idade, altura, peso, sexo);
+
+        pessoa.listarPessoa();
+
+        sc.close();
+    }
+}
